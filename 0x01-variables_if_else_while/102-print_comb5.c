b967e60cd65eda822b31e6c92dacf0bcc0b09b6f
SaraElsaiedy <saratolearn@gmail.com>
2024-02-10 08:48:59 -0800

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,32 +12,32 @@ int digit1, digit2, digit3, digit4;
 
 for (digit1 = '0'; digit1 <= '9'; ++digit1)
 {
-for (digit2 = '0'; digit2 <= '9'; ++digit2)
-{
-for (digit3 = '0'; digit3 <= '9'; ++digit3)
-{
-for (digit4 = '0'; digit4 <= '9'; ++digit4)
-{
-if (digit1 > digit3 || (digit1 == digit3 && digit2 >= digit4))
-{
-continue;
-}
-else
-{
-putchar(digit1);
-putchar(digit2);
-putchar(' ');
-putchar(digit3);
-putchar(digit4);
-if (!(digit1 == '9' && digit2 == '8' && digit3 == '9' && digit4 == '9'))
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-}
+	for (digit2 = '0'; digit2 <= '9'; ++digit2)
+	{
+		for (digit3 = '0'; digit3 <= '9'; ++digit3)
+		{
+			for (digit4 = '0'; digit4 <= '9'; ++digit4)
+			{
+				if (digit1 > digit3 || (digit1 == digit3 && digit2 >= digit4))
+				{
+					continue;
+				}
+				else
+				{
+					putchar(digit1);
+					putchar(digit2);
+					putchar(' ');
+					putchar(digit3);
+					putchar(digit4);
+					if (!(digit1 == '9' && digit2 == '8' && digit3 == '9' && digit4 == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 }
 putchar('\n');
 return (0);
